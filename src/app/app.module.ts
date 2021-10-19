a56fe0345e8eca2e847d10f46b42997cacdce16f
Jeff Combs <j.combs.design@gmail.com>
2021-10-19 00:47:47 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DonateComponent } from './donate/donate.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { AboutComponent } from './about/about.component';
+import { LeadershipComponent } from './leadership/leadership.component';
+import { VolunteerComponent } from './volunteer/volunteer.component';
+import { ContactComponent } from './contact/contact.component';
+import { EventsComponent } from './events/events.component';
+import { HeroComponent } from './front-page/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DonateComponent,
+    FrontPageComponent,
+    AboutComponent,
+    LeadershipComponent,
+    VolunteerComponent,
+    ContactComponent,
+    EventsComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
