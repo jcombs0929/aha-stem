69e72964ae63d2018d262a059950fe4af67f62ed
Jeff Combs <j.combs.design@gmail.com>
2021-12-09 22:19:33 -0600

    added routing, front-page, and header edits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { DonateComponent } from "./donate/donate.component";
+import { EventsComponent } from "./events/events.component";
+import { FrontPageComponent } from "./front-page/front-page.component";
+import { LeadershipComponent } from "./leadership/leadership.component";
+import { VolunteerComponent } from "./volunteer/volunteer.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: FrontPageComponent },
+  { path: "about", component: AboutComponent },
+  { path: "events", component: EventsComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "donate", component: DonateComponent },
+  { path: "leadership", component: LeadershipComponent },
+  { path: "volunteer", component: VolunteerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
