c400236f5cd3029e9fd2d77e0492c50dad8b633d
Jeff Combs <j.combs.design@gmail.com>
2021-12-23 23:22:02 -0600

    Hero: added interval function to update hero header text

--- src/app/front-page/hero/hero.component.ts
+++ src/app/front-page/hero/hero.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
-  selector: 'app-hero',
-  templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  selector: "app-hero",
+  templateUrl: "./hero.component.html",
+  styleUrls: ["./hero.component.scss"],
 })
 export class HeroComponent implements OnInit {
+  stemType: string;
+  stemTypeInterval: any;
 
-  constructor() { }
+  types: any = [
+    "Scientists",
+    "Technologists",
+    "Engineers",
+    "Mathematicians",
+    "You",
+  ];
 
-  ngOnInit(): void {
+  constructor() {
+    this.stemType = this.types[0];
+    this.getStemType();
   }
 
+  ngOnInit(): void {}
+
+  getStemType() {
+    let num = 0;
+    this.stemTypeInterval = setInterval(() => {
+      if (num > 3) num = 0;
+      else num += 1;
+      this.stemType = this.types[num];
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.stemTypeInterval);
+  }
 }
