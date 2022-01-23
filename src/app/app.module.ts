f762af01ee4fc7bb5492ab5474548d8d687d3e2d
Jeff Combs <j.combs.design@gmail.com>
2022-01-23 00:27:00 -0600

    removed ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { DonateComponent } from './donate/donate.component';
-import { FrontPageComponent } from './front-page/front-page.component';
-import { AboutComponent } from './about/about.component';
-import { LeadershipComponent } from './leadership/leadership.component';
-import { VolunteerComponent } from './volunteer/volunteer.component';
-import { ContactComponent } from './contact/contact.component';
-import { EventsComponent } from './events/events.component';
-import { HeroComponent } from './front-page/hero/hero.component';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { DonateComponent } from "./donate/donate.component";
+import { FrontPageComponent } from "./front-page/front-page.component";
+import { AboutComponent } from "./about/about.component";
+import { LeadershipComponent } from "./leadership/leadership.component";
+import { VolunteerComponent } from "./volunteer/volunteer.component";
+import { ContactComponent } from "./contact/contact.component";
+import { EventsComponent } from "./events/events.component";
+import { HeroComponent } from "./front-page/hero/hero.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -28,15 +27,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     VolunteerComponent,
     ContactComponent,
     EventsComponent,
-    HeroComponent
+    HeroComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ModalModule.forRoot(),
-    BrowserAnimationsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
